a101055664d2654068d148016b1efbb7a2d0a6a2
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2020-12-10 14:49:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ let counter = 0;
 let pieceRight = document.querySelector('.piece-right');
 let pieceLeft = document.querySelector('.piece-left');
 let playImg = document.querySelector('.play-img');
-    
+
+function counterFunction() {
+   return counter = Number(counter + (360/duration));
+};
+
 playImg.addEventListener('click', startFunction);
 function startFunction() {
     if(playImg.src === 'https://valeri-afk.github.io/Valeri-afk.github.io8/pause.png') {
@@ -17,13 +21,13 @@ function startFunction() {
         function rotateFunction() {
             video.play();
             if(counter < 180) {
-                counter = Number(counter + (360/duration));
+                counterFunction();
                 pieceRight.style.transform = 'rotate('+ Number(180 + counter) + 'deg)';
                 pieceLeft.classList.remove('piece-left-rotated');
                 pieceLeft.classList.add('piece-left');
             }
             else if (counter > 179 && counter < 360) {
-                counter = Number(counter + (360/duration));
+                counterFunction();
                 pieceRight.style.transform = 'rotate('+ Number(180 + counter) + 'deg)';
                 pieceLeft.classList.remove('piece-left');
                 pieceLeft.classList.add('piece-left-rotated');
