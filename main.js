2c1f16b7fdf92c8780da7ea5b50a825cf86c2857
Valeri-afk <72664176+Valeri-afk@users.noreply.github.com>
2020-12-10 12:49:27 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+let video = document.querySelector('.video');
+let duration = 3703.141587;
+let counter = 0;
+let pieceRight = document.querySelector('.piece-right');
+let pieceLeft = document.querySelector('.piece-left');
+let playImg = document.querySelector('.play-img');
+    
+playImg.addEventListener('click', startFunction);
+function startFunction() {
+    if(playImg.src === 'http://127.0.0.1:5500/pause.png') {
+        playImg.src = 'play.png';
+        video.pause();
+        clearInterval(stop);
+    }
+    else {
+        playImg.src = 'pause.png';
+        function rotateFunction() {
+            video.play();
+            if(counter < 180) {
+                counter = Number(counter + (360/duration));
+                pieceRight.style.transform = 'rotate('+ Number(180 + counter) + 'deg)';
+                pieceLeft.classList.remove('piece-left-rotated');
+                pieceLeft.classList.add('piece-left');
+            }
+            else if (counter > 179 && counter < 360) {
+                counter = Number(counter + (360/duration));
+                pieceRight.style.transform = 'rotate('+ Number(180 + counter) + 'deg)';
+                pieceLeft.classList.remove('piece-left');
+                pieceLeft.classList.add('piece-left-rotated');
+            }
+            else if (parseInt(counter) === 360) {
+                playImg.src = 'play.png';
+                counter = 0;
+                video.pause();
+                clearInterval(stop)
+            }
+        }
+        stop = setInterval(rotateFunction, 1000);
+    }
+}
---
+++ videos.js
@@ -0,0 +1,24 @@
+let videoImg = document.querySelectorAll('.video-img');
+let timeBtn = document.querySelectorAll('.time-btn');
+
+for(i=0;i<videoImg.length;i++) {
+videoImg[i].addEventListener('click', moonFunction);
+function moonFunction(event) {
+video.src = event.target.dataset.src;
+duration = event.target.dataset.duration;
+counter = 0;
+playImg.src = 'play.png';
+clearInterval(stop);
+} 
+}
+
+
+
+for(i=0; i<timeBtn.length; i++) {
+timeBtn[i].addEventListener('click', dropFunction);
+function dropFunction(event) {
+   video.currentTime = Number(event.target.dataset.min * 60);
+   counter = Number((360/duration)*video.currentTime);
+}
+}
+
